61bb01517fb16fe856d4a3b1988cb69f3b7dad41
polygonplanet <polygon.planet.aqua@gmail.com>
2016-06-07 21:33:58 +0900

    3.1.0

--- dist/chillout.js
+++ dist/chillout.js
@@ -1,5 +1,5 @@
 /*!
- * chillout v3.0.0 - Reduce CPU usage in JavaScript
+ * chillout v3.1.0 - Reduce CPU usage in JavaScript
  * Copyright (c) 2016 polygon planet <polygon.planet.aqua@gmail.com>
  * https://github.com/polygonplanet/chillout
  * @license MIT
@@ -13,16 +13,19 @@ Object.defineProperty(exports, "__esModule", {
 exports.forEach = forEach;
 exports.repeat = repeat;
 exports.till = till;
+exports.forOf = forOf;
 
 var _iterator = require('./iterator');
 
-var _iterator2 = _interopRequireDefault(_iterator);
+var iterator = _interopRequireWildcard(_iterator);
 
 var _iterate = require('./iterate');
 
 var _iterate2 = _interopRequireDefault(_iterate);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
 /**
  * Executes a provided function once per array or object element.
@@ -39,7 +42,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * @return {Promise} Return new Promise
  */
 function forEach(obj, callback, context) {
-  return (0, _iterate2.default)(_iterator2.default.forEach(obj, callback, context));
+  return (0, _iterate2.default)(iterator.forEach(obj, callback, context));
 }
 
 /**
@@ -52,14 +55,14 @@ function forEach(obj, callback, context) {
  * - start: The number of start
  * - step: The number of step
  * - end: The number of end
- * @param {Function} callback Function to execute for each times, taking an
+ * @param {Function} callback Function to execute for each times, taking one
  *   argument:
  * - i: The current number
  * @param {Object} [context] Value to use as `this` when executing callback
  * @return {Promise} Return new Promise
  */
 function repeat(count, callback, context) {
-  return (0, _iterate2.default)(_iterator2.default.repeat(count, callback, context));
+  return (0, _iterate2.default)(iterator.repeat(count, callback, context));
 }
 
 /**
@@ -71,7 +74,24 @@ function repeat(count, callback, context) {
  * @return {Promise} Return new Promise
  */
 function till(callback, context) {
-  return (0, _iterate2.default)(_iterator2.default.till(callback, context));
+  return (0, _iterate2.default)(iterator.till(callback, context));
+}
+
+/**
+ * Iterates the iterable objects, similar to the `for-of` statement.
+ * Executes a provided function once per element.
+ * The iteration will break if the callback function returns `false`, or an
+ * error occurs.
+ *
+ * @param {Array|string|Object} iterable Target iterable objects
+ * @param {Function} callback Function to execute for each element, taking
+ *   one argument:
+ * - value: A value of a property on each iteration
+ * @param {Object} [context] Value to use as `this` when executing callback
+ * @return {Promise} Return new Promise
+ */
+function forOf(iterable, callback, context) {
+  return (0, _iterate2.default)(iterator.forOf(iterable, callback, context));
 }
 
 },{"./iterate":2,"./iterator":3}],2:[function(require,module,exports){
@@ -174,77 +194,95 @@ Object.defineProperty(exports, "__esModule", {
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
 
+exports.forEach = forEach;
+exports.repeat = repeat;
+exports.till = till;
+exports.forOf = forOf;
+
 var _util = require('./util');
 
-var iterator = {
-  forEach: function forEach(obj, callback, context) {
-    var i = 0;
-    var len = void 0;
+function forEach(obj, callback, context) {
+  var i = 0;
+  var len = void 0;
 
-    if ((0, _util.isArrayLike)(obj)) {
-      len = obj.length;
-
-      return {
-        next: function next() {
-          if (i >= len) {
-            return false;
-          }
-          return callback.call(context, obj[i], i++, obj);
-        }
-      };
-    }
-
-    var keys = Object.keys(obj);
-    len = keys.length;
+  if ((0, _util.isArrayLike)(obj)) {
+    len = obj.length;
 
     return {
       next: function next() {
         if (i >= len) {
           return false;
         }
-
-        var key = keys[i++];
-        return callback.call(context, obj[key], key, obj);
-      }
-    };
-  },
-  repeat: function repeat(count, callback, context) {
-    var i = void 0,
-        step = void 0,
-        end = void 0;
-
-    if (count && (typeof count === 'undefined' ? 'undefined' : _typeof(count)) === 'object') {
-      i = count.start || 0;
-      step = count.step || 1;
-      end = count.end;
-    } else {
-      i = 0;
-      step = 1;
-      end = count;
-    }
-
-    return {
-      next: function next() {
-        var res = callback.call(context, i);
-
-        i += step;
-        if (i >= end) {
-          return false;
-        }
-        return res;
-      }
-    };
-  },
-  till: function till(callback, context) {
-    return {
-      next: function next() {
-        return callback.call(context);
+        return callback.call(context, obj[i], i++, obj);
       }
     };
   }
-};
 
-exports.default = iterator;
+  var keys = Object.keys(obj);
+  len = keys.length;
+
+  return {
+    next: function next() {
+      if (i >= len) {
+        return false;
+      }
+
+      var key = keys[i++];
+      return callback.call(context, obj[key], key, obj);
+    }
+  };
+}
+
+function repeat(count, callback, context) {
+  var i = void 0,
+      step = void 0,
+      end = void 0;
+
+  if (count && (typeof count === 'undefined' ? 'undefined' : _typeof(count)) === 'object') {
+    i = count.start || 0;
+    step = count.step || 1;
+    end = count.end;
+  } else {
+    i = 0;
+    step = 1;
+    end = count;
+  }
+
+  return {
+    next: function next() {
+      var res = callback.call(context, i);
+
+      i += step;
+      if (i >= end) {
+        return false;
+      }
+      return res;
+    }
+  };
+}
+
+function till(callback, context) {
+  return {
+    next: function next() {
+      return callback.call(context);
+    }
+  };
+}
+
+function forOf(iterable, callback, context) {
+  var it = iterable[Symbol.iterator]();
+
+  return {
+    next: function next() {
+      var res = it.next();
+
+      if (res.done) {
+        return false;
+      }
+      return callback.call(context, res.value, iterable);
+    }
+  };
+}
 
 },{"./util":5}],4:[function(require,module,exports){
 'use strict';
